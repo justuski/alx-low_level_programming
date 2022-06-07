37d4e23c23453ddd7013b4bed3851b8d9281f154
justuski <justuswu1@gmail.com>
2022-06-07 10:37:15 -0700

    8

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints the string "Holberton" from a character array.
+ * main - prints the string "_putchar" from a character array.
  * Return: 0 on success.
  */
 int main(void)
@@ -9,7 +9,7 @@ int main(void)
 char c[] = "_putchar";
 int i;
 
-for (i = 0; i < 9; i++)
+for (i = 0; i < 8; i++)
 {
 _putchar(c[i]);
 }
