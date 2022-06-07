6ecbc52ccee4f3777dcad867df1d0bd0eee01dbc
justuski <justuswu1@gmail.com>
2022-06-07 12:49:12 -0700

    correct

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,7 @@ for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
-res = (i * j);	      
+res = (i * j);
 if (j != 0)
 {
 _putchar(',');
@@ -30,7 +30,7 @@ else if (res >= 10 && res < 100)
 _putchar(' ');
 _putchar((res / 10) + '0');
 _putchar((res % 10) + '0');
-}	
+}
 else if (res >= 100 && j != 0)
 {
 _putchar((res / 100) + '0');
