46a289fe52da81a9d478ae5c5cca974d564d1021
justuski <justuswu1@gmail.com>
2022-07-06 21:25:15 +0300

    3-main.c changes

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,18 +1,33 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * main - follows instructions of input operation
- * @argc: count of inputs
- * @argv: array of inputs
- * Return: 0 success
- */
-int main(int argc, char **argv)
+  * main - ...
+  * @argc: ...
+  * @argv: ...
+  *
+  * Return: ...
+  */
+int main(int argc, char *argv[])
 {
-if (argc == 4)
+int (*oprt)(int, int);
+
+if (argc != 4)
 {
-printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
-return (0);
-}
 printf("Error\n");
-return (98);
+exit(98);
+}
+
+oprt = get_op_func(argv[2]);
+
+if (!oprt)
+{
+printf("Error\n");
+exit(99);
+}
+
+printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+return (0);
 }
 
