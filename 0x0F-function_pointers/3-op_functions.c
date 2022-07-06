696022e6b858a394f729f02311796e2992639315
justuski <justuswu1@gmail.com>
2022-07-06 21:21:31 +0300

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,58 +1,76 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * op_add - addition of a + b
- * @a: input integer a
- * @b: input integer b
- * Return: sum
- */
+  * op_add - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_add(int a, int b)
 {
 return (a + b);
 }
+
 /**
- * op_sub - subtraction of a - b
- * @a: input integer a
- * @b: input integer b
- * Return: difference
- */
+  * op_sub - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_sub(int a, int b)
 {
 return (a - b);
 }
+
 /**
- * op_mul - multiplication of a * b
- * @a: input integer a
- * @b: input integer b
- * Return: product
- */
+  * op_mul - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_mul(int a, int b)
 {
 return (a * b);
 }
+
 /**
- * op_div - division of a / b
- * @a: input integer a
- * @b: input integer b
- * Return: quotient
- */
+  * op_div - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_div(int a, int b)
 {
-if (b != 0)
-return (a / b);
+if (b == 0)
+{
 printf("Error\n");
 exit(100);
 }
+
+return (a / b);
+}
+
 /**
- * op_mod - modulo of a / b
- * @a: input integer a
- * @b: input integer b
- * Return: remainder
- */
+  * op_mod - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_mod(int a, int b)
 {
-if (b != 0)
-return (a % b);
+if (b == 0)
+{
 printf("Error\n");
 exit(100);
+}
+
+return (a % b);
 }
 
